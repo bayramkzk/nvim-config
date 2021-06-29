e7a09748d0f966055375324c427483daa931ea8c
poenitet <48856944+poenitet@users.noreply.github.com>
2021-06-30 02:55:54 +0300

    Add packer's itself and treesitter to plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,5 +1,8 @@
-vim.cmd [[packadd packer.nvim]]
+vim.cmd 'autocmd BufWritePost plugins.lua PackerCompile' -- Auto compile when there are changes in plugins.lua
 
-return require('packer').startup(function()
+return require('packer').startup(function(use)
+    use 'wbthomason/packer.nvim'
+    use 'nvim-treesitter/nvim-treesitter'
     use 'chriskempson/base16-vim'
 end)
+
