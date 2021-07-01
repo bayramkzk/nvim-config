e4ce06d2585ab8ba887293e055b510bd03a9474e
poenitet <48856944+poenitet@users.noreply.github.com>
2021-07-01 20:59:46 +0300

    Update settings.lua to enable relativenumber and disable showmode

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,8 +1,15 @@
+-- Enable relative line numbers
 vim.o.number = true
+vim.o.relativenumber = true
 
+-- Use 4 spaces for indents
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
 
+-- Mouse support
 vim.o.mouse = 'a'
+
+-- Disable showing current mode (e.g -- INSERT --) in the most bottom line
+vim.o.showmode = false
 
