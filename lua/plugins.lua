a59a529e5ccb61d2a439e140788e2717bf139ac2
poenitet <48856944+poenitet@users.noreply.github.com>
2021-07-01 18:48:48 +0300

    Add barbar.nvim as tabline

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,11 +1,12 @@
+-- Install packer if not installed
 local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
   vim.fn.system {'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path}
   vim.api.nvim_command 'packadd packer.nvim'
 end
 
-vim.cmd 'autocmd BufWritePost plugins.lua PackerCompile' -- Auto compile when there are changes in plugins.lua
+-- Auto compile when there are changes in plugins.lua
+vim.cmd 'autocmd BufWritePost plugins.lua PackerCompile'
 
 return require('packer').startup(function(use)
     -- Packer can manage itself
@@ -14,13 +15,19 @@ return require('packer').startup(function(use)
     -- Treesitter
     use 'nvim-treesitter/nvim-treesitter'
 
-    -- Statusline
-    use {
-      'hoob3rt/lualine.nvim',
-      requires = {'kyazdani42/nvim-web-devicons', opt = true}
-    }
-
     -- Colorscheme
     use 'folke/tokyonight.nvim'
+
+    -- Statusline
+    use {
+        'hoob3rt/lualine.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    }
+
+    -- Tabline
+    use {
+        'romgrk/barbar.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    }
 end)
 
