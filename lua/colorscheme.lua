580d41b9f751d9694a3b1b47a5d0dba52ab41982
poenitet <48856944+poenitet@users.noreply.github.com>
2021-07-01 13:24:08 +0300

    Update colorscheme.lua to use dark or light theme depending on time

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,3 +1,14 @@
 vim.o.termguicolors = true
-vim.cmd 'colorscheme base16-twilight'
+local dark_colorscheme = 'base16-twilight'
+local light_colorscheme = 'base16-gruvbox-light-soft'
+
+-- set dark or light colorscheme depending on time
+local current = os.date("*t", os.time())
+local colorscheme = nil
+if current.hour < 19 and current.hour > 7 then
+    colorscheme = light_colorscheme
+else
+    colorscheme = dark_colorscheme
+end
+vim.cmd('colorscheme ' .. colorscheme)
 
