063556fddf93d71ae907a4e67782aedab77df805
poenitet <48856944+poenitet@users.noreply.github.com>
2021-07-25 21:07:06 +0300

    Add LSP setup configs for python and lua

--- lua/lsp-settings.lua
+++ lua/lsp-settings.lua
@@ -1,3 +1,34 @@
 local lspconfig = require('lspconfig')
 
+-- Typescript/Javascript language server [typescript-language-server]
 lspconfig.tsserver.setup{}
+
+-- Python language server [pyright]
+lspconfig.pyright.setup{}
+
+-- Lua language server [lua-language-server]
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, "lua/?.lua")
+table.insert(runtime_path, "lua/?/init.lua")
+
+lspconfig.sumneko_lua.setup {
+    -- TODO dynamically get the path
+    cmd = { "/usr/bin/lua-language-server" };
+    settings = {
+        Lua = {
+            runtime = {
+                version = 'LuaJIT',
+                path = runtime_path,
+            },
+            diagnostics = {
+                globals = { 'vim' },
+            },
+            workspace = {
+                library = vim.api.nvim_get_runtime_file("", true),
+            },
+            telemetry = {
+                enable = false,
+            },
+        },
+    },
+}
