6a15f3a1fc21a6b9a9077d185b42ec4d7e1159c0
poenitet <48856944+poenitet@users.noreply.github.com>
2021-07-25 20:55:54 +0300

    Refactor colorscheme.lua

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,13 +1,20 @@
-vim.o.termguicolors = true
-
--- Set dark or light colorscheme depending on time
-local current = os.date("*t", os.time())
-local is_day = current.hour <= 18 and current.hour >= 6
-if is_day then
-    vim.g.tokyonight_style = "day"
-else
-    vim.g.tokyonight_style = "storm"
+local function check_is_day()
+    local current = os.date("*t", os.time())
+    local is_day = current.hour <= 18 and current.hour >= 6
+    return is_day
 end
 
-vim.cmd[[colorscheme tokyonight]]
+local function get_tokyo_style()
+    if check_is_day() then
+        return "storm"
+    end
+    return "night"
+end
 
+vim.g.tokyonight_style = get_tokyo_style()
+
+vim.o.termguicolors = true
+
+vim.g.tokyonight_italic_keywords = false
+
+vim.cmd[[colorscheme tokyonight]]
