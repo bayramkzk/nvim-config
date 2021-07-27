5166a6381d0b94936b0b0e854086bcfc77e8f5bf
poenitet <48856944+poenitet@users.noreply.github.com>
2021-07-27 19:36:23 +0300

    Add file hightlighting for git to file-tree.lua

--- lua/file-tree.lua
+++ lua/file-tree.lua
@@ -5,6 +5,9 @@ vim.g.nvim_tree_width = 40
 -- Automatically close nvim-tree when it is the last window
 vim.g.nvim_tree_auto_close = true
 
+-- Enable file highlight for git attributes
+vim.g.nvim_tree_git_hl = 1
+
 vim.g.nvim_tree_icons = {
     default = '',
     symlink = '',
