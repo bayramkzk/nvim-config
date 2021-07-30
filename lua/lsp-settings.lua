c1171bcdf526e853912408c98979e4bd986ef202
poenitet <48856944+poenitet@users.noreply.github.com>
2021-07-30 19:00:46 +0300

    Add LSP setup for JSON

--- lua/lsp-settings.lua
+++ lua/lsp-settings.lua
@@ -32,3 +32,14 @@ lspconfig.sumneko_lua.setup {
         },
     },
 }
+
+-- JSON language server [vscode-langservers-extracted]
+lspconfig.jsonls.setup {
+    commands = {
+      Format = {
+        function()
+          vim.lsp.buf.range_formatting({},{0,0},{vim.fn.line("$"),0})
+        end
+      }
+    }
+}
