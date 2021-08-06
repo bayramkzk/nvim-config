87645ac7f891fcede73c69cbf723f55326db37db
poenitet <48856944+poenitet@users.noreply.github.com>
2021-08-06 20:45:53 +0300

    Set up lualine plugin

--- init.lua
+++ init.lua
@@ -53,4 +53,36 @@ require('packer').startup(function(use)
             }
         end
     }
+
+    -- Status line
+    use {
+        'hoob3rt/lualine.nvim',
+        requires = 'kyazdani42/nvim-web-devicons',
+        config = function()
+            require('lualine').setup {
+                options = {
+                    icons_enabled = true,
+                    disabled_filetypes = {}
+                },
+                sections = {
+                    lualine_a = {'mode'},
+                    lualine_b = {'branch'},
+                    lualine_c = {{'filename', path = 1}},
+                    lualine_x = {'filetype', 'encoding', 'fileformat'},
+                    lualine_y = {'progress'},
+                    lualine_z = {'location'}
+                },
+                inactive_sections = {
+                    lualine_a = {},
+                    lualine_b = {},
+                    lualine_c = {'filename'},
+                    lualine_x = {'location'},
+                    lualine_y = {},
+                    lualine_z = {}
+                },
+                tabline = {},
+                extensions = { "nvim-tree" }
+            }
+        end
+    }
 end)
