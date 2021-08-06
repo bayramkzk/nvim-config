97d26104c2a2e3077936975031bb2774236dbab4
poenitet <48856944+poenitet@users.noreply.github.com>
2021-08-06 20:33:19 +0300

    Set up nvim-tree plugin

--- init.lua
+++ init.lua
@@ -14,6 +14,43 @@ vim.cmd[[colorscheme slate]]
 --[[ PLUGINS ]]--
 require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
-    use 'kyazdani42/nvim-web-devicons'
-    use 'kyazdani42/nvim-tree.lua'
+
+    -- File explorer
+    use {
+        'kyazdani42/nvim-tree.lua',
+        requires = 'kyazdani42/nvim-web-devicons',
+        config = function()
+            vim.g.nvim_tree_auto_close = true
+            vim.g.nvim_tree_git_hl = true
+            vim.g.nvim_tree_icons = {
+                default = "",
+                symlink = "",
+                git = {
+                    unstaged = "✗",
+                    staged = "✓",
+                    unmerged = "",
+                    renamed = "➜",
+                    untracked = "★",
+                    deleted = "",
+                    ignored = "◌"
+                },
+                folder = {
+                    arrow_open = "",
+                    arrow_closed = "",
+                    default = "",
+                    open = "",
+                    empty = "",
+                    empty_open = "",
+                    symlink = "",
+                    symlink_open = "",
+                },
+                lsp = {
+                    hint = "",
+                    info = "",
+                    warning = "",
+                    error = "",
+                }
+            }
+        end
+    }
 end)
