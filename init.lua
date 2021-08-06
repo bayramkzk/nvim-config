9b2cba3d00d8b924b148f9f36b3e3e0e9908a1a5
poenitet <48856944+poenitet@users.noreply.github.com>
2021-08-07 02:39:15 +0300

    Update nvim-tree width and side

--- init.lua
+++ init.lua
@@ -23,6 +23,8 @@ require('packer').startup(function(use)
         'kyazdani42/nvim-tree.lua',
         requires = 'kyazdani42/nvim-web-devicons',
         config = function()
+            vim.g.nvim_tree_width = 36
+            vim.g.nvim_tree_side = "right"
             vim.g.nvim_tree_auto_close = true
             vim.g.nvim_tree_git_hl = true
             vim.g.nvim_tree_icons = {
