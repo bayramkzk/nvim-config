76d86d53a715ba7c2ac5a080e61f11609eaeeb25
poenitet <48856944+poenitet@users.noreply.github.com>
2021-08-10 19:43:41 +0300

    Set up nvim-lsputils for better lsp menus

--- init.lua
+++ init.lua
@@ -258,6 +258,22 @@ require('packer').startup(function(use)
         end
     }
 
+    -- Better lsp menus
+    use {
+        'RishabhRD/nvim-lsputils',
+        requires = 'RishabhRD/popfix',
+        config = function ()
+            vim.lsp.handlers['textDocument/codeAction'] = require'lsputil.codeAction'.code_action_handler
+            vim.lsp.handlers['textDocument/references'] = require'lsputil.locations'.references_handler
+            vim.lsp.handlers['textDocument/definition'] = require'lsputil.locations'.definition_handler
+            vim.lsp.handlers['textDocument/declaration'] = require'lsputil.locations'.declaration_handler
+            vim.lsp.handlers['textDocument/typeDefinition'] = require'lsputil.locations'.typeDefinition_handler
+            vim.lsp.handlers['textDocument/implementation'] = require'lsputil.locations'.implementation_handler
+            vim.lsp.handlers['textDocument/documentSymbol'] = require'lsputil.symbols'.document_handler
+            vim.lsp.handlers['workspace/symbol'] = require'lsputil.symbols'.workspace_handler
+        end
+    }
+
     -- Auto closing pairs
     use {
         'windwp/nvim-autopairs',
