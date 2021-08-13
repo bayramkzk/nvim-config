66e4db07b38ed79962d82a821de8ef21e2303f30
poenitet <48856944+poenitet@users.noreply.github.com>
2021-08-14 02:43:39 +0300

    Set up telescope.nvim for fuzzy finding and listing git commits

--- init.lua
+++ init.lua
@@ -275,6 +275,58 @@ require('packer').startup(function(use)
         end
     }
 
+    -- List and preview with a modal dialog
+    use {
+      'nvim-telescope/telescope.nvim',
+      requires = 'nvim-lua/plenary.nvim',
+      config = function ()
+          require('telescope').setup{
+          defaults = {
+            vimgrep_arguments = {
+                'rg',
+                '--color=never',
+                '--no-heading',
+                '--with-filename',
+                '--line-number',
+                '--column',
+                '--smart-case'
+            },
+            prompt_prefix = "> ",
+            selection_caret = "> ",
+            entry_prefix = "  ",
+            initial_mode = "insert",
+            selection_strategy = "reset",
+            sorting_strategy = "descending",
+            layout_strategy = "horizontal",
+            layout_config = {
+                horizontal = {
+                    mirror = false,
+                },
+                vertical = {
+                    mirror = false,
+                },
+            },
+            file_sorter =  require'telescope.sorters'.get_fuzzy_file,
+            file_ignore_patterns = {},
+            generic_sorter =  require'telescope.sorters'.get_generic_fuzzy_sorter,
+            winblend = 0,
+            border = {},
+            borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
+            color_devicons = true,
+            use_less = true,
+            path_display = {},
+            set_env = { ['COLORTERM'] = 'truecolor' }, -- default = nil,
+            file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
+            grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new,
+            qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
+
+            -- Developer configurations: Not meant for general override
+            buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker
+          }
+        }
+      end
+    }
+
     -- Auto closing pairs
     use {
         'windwp/nvim-autopairs',
@@ -377,3 +429,7 @@ vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.CompeComplete()", { expr = true })
 vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.CompeComplete()", { expr = true })
 vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.CompeReverseComplete()", { expr = true })
 vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.CompeReverseComplete()", { expr = true })
+
+-- Telescope
+vim.api.nvim_set_keymap("n", "<LEADER><LEADER>", ":Telescope find_files<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<LEADER>gc", ":Telescope git_commits<CR>", { noremap = true, silent = true })
