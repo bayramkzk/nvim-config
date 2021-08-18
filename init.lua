fd239a997b9ed4e4618fb8325d9833d6e9d72ff5
poenitet <48856944+poenitet@users.noreply.github.com>
2021-08-19 00:57:35 +0300

    Auto add parentheses when completing a function

--- init.lua
+++ init.lua
@@ -335,6 +335,11 @@ require("packer").startup(
       "windwp/nvim-autopairs",
       config = function()
         require("nvim-autopairs").setup {}
+
+        require("nvim-autopairs.completion.compe").setup {
+          map_cr = true,
+          map_complete = true
+        }
       end
     }
 
