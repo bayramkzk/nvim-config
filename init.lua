d68111c3fc0b48e70427aad94eeb567584a45cae
poenitet <48856944+poenitet@users.noreply.github.com>
2021-08-19 01:32:48 +0300

    Customize LSP diagnostic icons

--- init.lua
+++ init.lua
@@ -203,6 +203,12 @@ require("packer").startup(
             treesitter = true
           }
         }
+
+        local signs = {Error = " ", Warning = " ", Hint = " ", Information = " "}
+        for type, icon in pairs(signs) do
+          local hl = "LspDiagnosticsSign" .. type
+          vim.fn.sign_define(hl, {text = icon, texthl = hl, numhl = ""})
+        end
       end
     }
 
