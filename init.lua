f0c4ef1b992b2de70eeae881af0100cdc3393e76
poenitet <48856944+poenitet@users.noreply.github.com>
2021-08-25 17:01:07 +0300

    Add border to nvim-compe

--- init.lua
+++ init.lua
@@ -200,6 +200,7 @@ require("packer").startup(
           max_kind_width = 100,
           max_menu_width = 100,
           documentation = {
+            border = "solid", -- the border option is the same as `|help nvim_open_win|`
             winhighlight = "NormalFloat:CompeDocumentation,FloatBorder:CompeDocumentationBorder",
             max_width = 120,
             min_width = 60,
