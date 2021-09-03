ef81dafe94c10fb51c1b9de34333340e477d9b43
bayram-kazik <48856944+bayram-kazik@users.noreply.github.com>
2021-09-03 18:33:33 +0300

    Refactor lspinstall-cf.lua

--- lua/plugins/lspinstall-cf.lua
+++ lua/plugins/lspinstall-cf.lua
@@ -1,6 +1,8 @@
-require'lspinstall'.setup() -- important
+local lspconfig = require("lspconfig")
+local lspinstall = require("lspinstall")
+lspinstall.setup()
 
-local servers = require'lspinstall'.installed_servers()
+local servers = lspinstall.installed_servers()
 for _, server in pairs(servers) do
-  require'lspconfig'[server].setup{}
+  lspconfig[server].setup{}
 end
